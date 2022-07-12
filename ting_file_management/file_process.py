d19a5d66de6a42f686062e6a67bae76a109df4f5
Kristiano <kristianokasper@gmail.com>
2022-07-12 18:47:36 -0300

    Req 04

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -20,7 +20,14 @@ def process(path_file, instance):
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance) == 0:
+        sys.stdout.write("Não há elementos\n")
+        return None
+
+    file = instance.dequeue()
+    sys.stdout.write(
+        f"Arquivo {file['nome_do_arquivo']} removido com sucesso\n"
+    )
 
 
 def file_metadata(instance, position):
