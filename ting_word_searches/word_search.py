9886898e80576405081e3d10158896a75961e66d
Kristiano <kristianokasper@gmail.com>
2022-07-12 21:21:29 -0300

    Req 07

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -17,4 +17,18 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    word_list = []
+    occurences = []
+
+    for index, line in enumerate(instance._data[0]["linhas_do_arquivo"]):
+        if word.lower() in line.lower():
+            occurences.append({"linha": index + 1, "conteudo": line})
+
+    if len(occurences) > 0:
+        word_list.append({
+            "palavra": word,
+            "arquivo": instance._data[0]["nome_do_arquivo"],
+            "ocorrencias": occurences,
+        })
+
+    return word_list
